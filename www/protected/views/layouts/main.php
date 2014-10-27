86135fcca8c18375f7f29a7e0da8bba8d3684f25
Joko Putranto <akun.joko.putranto@gmail.com>
2014-10-27 11:19:46 +0700

    fix year

--- www/protected/views/layouts/main.php
+++ www/protected/views/layouts/main.php
@@ -46,7 +46,7 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; 2014 by My Company.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
