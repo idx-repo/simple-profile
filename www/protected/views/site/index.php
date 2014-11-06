1c1df47937eb1c9bedb9372d91678de5f3192eee
Joko Putranto <akun.joko.putranto@gmail.com>
2014-11-06 15:10:29 +0700

    site index

--- www/protected/views/site/index.php
+++ www/protected/views/site/index.php
@@ -4,17 +4,10 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<h1>It's a simple <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+<p>Congratulations! You have successfully install this application.</p>
 
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<p>For more details on how to further develop this application, please go to
+<a href="https://github.com/idx-repo/simple-profile">github page</a>.
+Feel free to ask, should you have any questions.</p>
